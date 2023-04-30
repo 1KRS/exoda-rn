c40a3f29933a82bb966c27d931638c12d1cfdc37
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-04-30 22:04:35 +0200

    Creating an Overall Form

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,10 +1,27 @@
-import { TextInput, View } from 'react-native';
+import { View } from 'react-native';
 import Input from './Input';
 
 const ExpenseForm = () => {
+const amountChangeHandler = () => {
+  
+}
+
+
   return (
     <View>
+      <Input label="Description" textInputConfig={{
+        keyboardType: 'decimal-pad',
+        onChangeText: amountChangeHandler,
+      }}/>
+      <Input label="Amount" textInputConfig={{
+        placeholder: 'DD/MM/YYYY',
+        maxLength: 10,
+        onChangeText: () => {},
 
+      }}/>
+      <Input label="Date" textInputConfig={{
+        
+      }}/>
     </View>
   );
 };
